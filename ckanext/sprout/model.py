74e4f84f3075584469a6d81f98bd2d7c1447484d
Hristijan Vilos <hristijan.vilos@protonmail.com>
2022-05-16 20:24:01 +0200

    Add "get_question_and_anwser" classmethod

--- ckanext/sprout/model.py
+++ ckanext/sprout/model.py
@@ -1,14 +1,13 @@
 # encoding: utf-8
-import logging 
+import logging
+import datetime
 
 import sqlalchemy as sa
-from sqlalchemy.dialects.postgresql import JSONB
-from sqlalchemy.ext.mutable import MutableDict
 
+from ckan import model
 from ckan.model import meta
 from ckan.model import types as _types
 from ckan.model import domain_object
-
 
 
 log = logging.getLogger(__name__)
@@ -18,24 +17,29 @@ __all__ = ['Questions', 'questions']
 
 
 questions = sa.Table('questions', meta.metadata,
-            sa.Column('id', sa.types.UnicodeText, primary_key=True,
-                default=_types.make_uuid),
-            sa.Column('user_id', sa.types.UnicodeText,
-                nullable=False),
-            sa.Column('resource_id', sa.types.UnicodeText,
-                sa.ForeignKey('resource.id')),
-            sa.Column('questions_answers', MutableDict.as_mutable(JSONB))
-              )
-            
+                     sa.Column('id', sa.types.UnicodeText, primary_key=True,
+                               default=_types.make_uuid),
+                     sa.Column('user_id', sa.types.UnicodeText,
+                               nullable=False),
+                     sa.Column('resource_id', sa.types.UnicodeText,
+                               nullable=False),
+                     sa.Column('questions_answers', _types.JsonDictType),
+                     sa.Column('created', sa.types.DateTime,
+                               default=datetime.datetime.now))
+
+
 class Questions(domain_object.DomainObject):
     @classmethod
-    def get_question_and_anwsers(cls, user_id, resource_id):
-        pass
+    def get_question_and_anwser(cls, user_id, resource_id):
+        query = model.Session.query(cls).\
+            filter_by(user_id=user_id, resource_id=resource_id).first()
+        return query
 
 
 def setup():
     """Create the questions table"""
     questions.create(checkfirst=True)
+
 
 def drop():
     """Drops the questions table"""
