588b93e5daa2b7637ca087caae2757fcc97b3865
duskobogdanovski <dule.bogdanovski@gmail.com>
2022-05-21 00:23:45 +0200

    Fix show only featured pages on homepage

--- ckanext/sprout/helpers.py
+++ ckanext/sprout/helpers.py
@@ -7,5 +7,6 @@ def get_featured_pages(limit=3):
     data_dict = {'org_id': None, 'page_type': 'page'}
     context = {'model': model, 'user': c.user,
                'auth_user_obj': c.userobj}
-    pages = p.toolkit.get_action('ckanext_pages_list')(context, data_dict)[:limit]
-    return pages
+    pages = p.toolkit.get_action('ckanext_pages_list')(context, data_dict)
+    featured = [page for page in pages if page['featured']]
+    return featured[:limit]
