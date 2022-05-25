fba9490a3dead2e2680a16a64c2510c49e355ac5
duskobogdanovski <dule.bogdanovski@gmail.com>
2022-05-25 12:00:52 +0200

    Show blogs instead of pages on featured section

--- ckanext/sprout/helpers.py
+++ ckanext/sprout/helpers.py
@@ -4,7 +4,7 @@ from ckan.common import c
 
 
 def get_featured_pages(limit=3):
-    data_dict = {'org_id': None, 'page_type': 'page'}
+    data_dict = {'org_id': None, 'page_type': 'blog'}
     context = {'model': model, 'user': c.user,
                'auth_user_obj': c.userobj}
     pages = p.toolkit.get_action('ckanext_pages_list')(context, data_dict)
