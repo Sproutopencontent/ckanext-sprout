1f2400cc545944be4c7e22b15e7d0580c94a8b28
Aleksandar Atanasov <iace2010@hotmail.com>
2022-05-26 16:47:03 +0200

    show resource size in additional info, show max size for upload resource in description

--- ckanext/sprout/helpers.py
+++ ckanext/sprout/helpers.py
@@ -1,6 +1,7 @@
 from ckan import model
 import ckan.plugins as p
 from ckan.common import c
+import ckan.lib.uploader as uploader
 
 
 def get_featured_pages(limit=3):
@@ -10,3 +11,14 @@ def get_featured_pages(limit=3):
     pages = p.toolkit.get_action('ckanext_pages_list')(context, data_dict)
     featured = [page for page in pages if page['featured']]
     return featured[:limit]
+
+
+def resource_display_size(resource_dict_size):
+    resource_size = resource_dict_size.get('size', None)
+    resource_size_string = '0 bytes' if resource_size is None else '{} bytes'.format(str(resource_size))
+    return resource_size_string
+
+
+def resource_max_size():
+    max_size_int = uploader.get_max_resource_size()
+    return ' Maximum allowed size of the file to upload is {} MB.'.format(str(max_size_int))
