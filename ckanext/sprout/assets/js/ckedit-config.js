42f7765814e2b20bdbbcbb645a0106fb71d8a192
duskobogdanovski <dule.bogdanovski@gmail.com>
2022-06-13 12:22:12 +0200

    Remove image preview text in ckeditor

---
+++ ckanext/sprout/assets/js/ckedit-config.js
@@ -0,0 +1,10 @@
+this.ckan = this.ckan || {};
+this.ckan.pages = this.ckan.pages || {};
+
+$(document).ready(function() {
+
+  window.ckan.pages.override_config = {
+    image_previewText: ' '
+  }
+
+});
