dd50d143bef909fbbea4f969e5b4bde0b485c7a6
Michael Meisel <mrmeisel@gmail.com>
2022-10-18 10:09:03 -0700

    Throttle forecast generation
    Add a new config setting (ckan.sprout.forecast_lifetime_in_hours)
    to set the minimum amount of time that must elapse before generating
    a new forecast in the same weatherset.

--- ckanext/sprout/forecaster_blueprint.py
+++ ckanext/sprout/forecaster_blueprint.py
@@ -2,19 +2,53 @@ from ckan.common import config
 from ckan.lib.helpers import get_display_timezone
 from ckan.plugins import toolkit
 import codecs
-from datetime import datetime
+from datetime import datetime, timedelta
 import flask
 import requests
 from .forecaster import Forecaster
 
 forecaster_blueprint = flask.Blueprint('forecaster', __name__)
 
+def _get_most_recent_forecast(dataset):
+    most_recent_forecast = None
+
+    for res in dataset['resources']:
+        if res['id'] == dataset['locations_resource_id']:
+            # TODO: it would be nice to have some explicit field indicating this is a forecast
+            # Right now we're just skipping the locations resource and considering everything else
+            continue
+        elif most_recent_forecast is None:
+            most_recent_forecast = res
+        else:
+            created_at = datetime.fromisoformat(res['created'])
+            most_recent_created_at = datetime.fromisoformat(most_recent_forecast['created'])
+            if created_at > most_recent_created_at:
+                most_recent_forecast = res
+
+    return most_recent_forecast
+
 def new_forecast(id):
     tz = get_display_timezone()
     dataset = toolkit.get_action('package_show')(None, {'id': id})
     api_key = config.get('ckan.sprout.tomorrow_api_key', None)
+    forecast_lifetime_h = config.get('ckan.sprout.forecast_lifetime_in_hours', 0)
     forecaster = Forecaster(api_key=api_key, languages=dataset['language'], timezone=tz)
-    create_date = datetime.utcnow().isoformat(sep=' ', timespec='minutes', tzinfo=tz)
+    now = datetime.utcnow()
+    create_date = now.isoformat(sep=' ', timespec='minutes', tzinfo=tz)
+    most_recent_forecast = _get_most_recent_forecast(dataset)
+
+    # Look to see if the most recent forecast is still "live" (based on the lifetime config value).
+    # If one already exists, just redirect there.
+    if most_recent_forecast is not None:
+        most_recent_created_at = datetime.fromisoformat(most_recent_forecast['created'])
+        if now - most_recent_created_at < timedelta(hours=forecast_lifetime_h):
+            # TODO: show a message on redirect to existing forecast to draw attention to the fact
+            # that we didn't generate a new one
+            return toolkit.redirect_to(
+                'weatherset_resource.read',
+                id=id,
+                resource_id=most_recent_forecast['id']
+            )
 
     # TODO: produce a proper error message if the location_resource can't be found or isn't set
     locations_resource = toolkit.get_action('resource_show')(None, {
@@ -30,7 +64,6 @@ def new_forecast(id):
     )
     forecasts = forecaster.run(codecs.iterdecode(locations_response.iter_lines(), 'utf-8'))
 
-    # TODO: can we just replace the resource_create action for this package type?
     resource = toolkit.get_action('resource_create')(None, {
         'package_id': id,
         'name': f'{toolkit._("Forecasts")} {create_date}',
@@ -40,12 +73,15 @@ def new_forecast(id):
     datastore_create = toolkit.get_action('datastore_create')
 
     for forecast in forecasts:
+        # Add one row at a time so we don't have to store everything in memory
         datastore_create(None, {
             'resource_id': resource['id'],
             'records': [forecast],
             'force': True
         })
 
+    # Add the resource views (like the data preview), they don't get added to empty resources on
+    # creation
     toolkit.get_action('resource_create_default_resource_views')(None, {
         'resource': resource,
         'package': dataset,
