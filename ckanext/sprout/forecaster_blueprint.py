13152847667dfb24a78f5092627805ef0aed496c
Michael Meisel <mrmeisel@gmail.com>
2022-10-31 15:32:13 -0700

    Attempt to do some error handling for forecasts

--- ckanext/sprout/forecaster_blueprint.py
+++ ckanext/sprout/forecaster_blueprint.py
@@ -1,5 +1,5 @@
 from ckan.common import config
-from ckan.lib.helpers import date_str_to_datetime, get_display_timezone, render_datetime
+import ckan.lib.helpers as h
 from ckan.plugins import toolkit
 import codecs
 from datetime import datetime, timedelta
@@ -21,8 +21,8 @@ def _get_most_recent_forecast(dataset):
         elif most_recent_forecast is None:
             most_recent_forecast = res
         else:
-            created_at = date_str_to_datetime(res['created'])
-            most_recent_created_at = date_str_to_datetime(most_recent_forecast['created'])
+            created_at = h.date_str_to_datetime(res['created'])
+            most_recent_created_at = h.date_str_to_datetime(most_recent_forecast['created'])
             if created_at > most_recent_created_at:
                 most_recent_forecast = res
 
@@ -38,65 +38,78 @@ def new_forecast(id):
     # Look to see if the most recent forecast is still "live" (based on the lifetime config value).
     # If one already exists, just redirect there.
     if most_recent_forecast is not None:
-        most_recent_created_at = date_str_to_datetime(most_recent_forecast['created'])
+        most_recent_created_at = h.date_str_to_datetime(most_recent_forecast['created'])
         if now - most_recent_created_at < timedelta(hours=forecast_lifetime_h):
-            # TODO: show a message on redirect to existing forecast to draw attention to the fact
-            # that we didn't generate a new one
+            h.flash_notice(toolkit._('Showing you the latest forecast, which is still fresh'))
             return toolkit.redirect_to(
                 'weatherset_resource.read',
                 id=id,
                 resource_id=most_recent_forecast['id']
             )
 
+    if not dataset['locations_resource_id']:
+        h.flash_error(toolkit._('Please set locations_resource_id before generating a forecast'))
+        return toolkit.redirect_to('weatherset.read', id=id)
+
     resource = toolkit.get_action('resource_create')(None, {
         'package_id': dataset['id'],
-        'name': f'{toolkit._("Forecasts")} {render_datetime(now, with_hours=True)}',
+        'name': f'{toolkit._("Forecasts")} {h.render_datetime(now, with_hours=True)}',
         'format': 'csv',
         'language': dataset['language']
     })
 
-    toolkit.enqueue_job(forecaster_job, [dataset, resource], title='forecaster', rq_kwargs={'timeout': 1200})
+    toolkit.enqueue_job(
+        forecaster_job,
+        # Pass along the cookies from this request so we stay authenticated
+        [dataset, resource, toolkit.request.cookies],
+        title='forecaster',
+        rq_kwargs={'timeout': 1200}
+    )
     return toolkit.redirect_to('weatherset_resource.read', id=id, resource_id=resource["id"])
 
-def forecaster_job(dataset, resource):
-    api_key = config.get('ckan.sprout.tomorrow_api_key', None)
-    # TODO: this doesn't seem to be returning the same thing the helper function uses
-    tz = get_display_timezone()
-    forecaster = Forecaster(api_key=api_key, languages=dataset['language'], timezone=tz)
-    # TODO: produce a proper error message if the location_resource can't be found or isn't set
-    locations_resource = toolkit.get_action('resource_show')(None, {
-        'id': dataset['locations_resource_id']
-    })
-
-    # TODO: handle failures here gracefully too
-    # Pass along the cookies from this request so we stay authenticated
-    locations_response = requests.get(
-        locations_resource['url'],
-        cookies=toolkit.request.cookies,
-        stream=True
-    )
-
-    forecasts = forecaster.run(codecs.iterdecode(locations_response.iter_lines(), 'utf-8'))
-
-    datastore_create = toolkit.get_action('datastore_create')
-
-    for forecast in forecasts:
-        # Add one row at a time so we don't have to store everything in memory
-        datastore_create(None, {
-            'resource_id': resource['id'],
-            'records': [forecast],
-            'force': True
+def forecaster_job(dataset, resource, cookies):
+    try:
+        api_key = config.get('ckan.sprout.tomorrow_api_key', None)
+        # TODO: this doesn't seem to be returning the same thing the helper function uses
+        tz = h.get_display_timezone()
+        forecaster = Forecaster(api_key=api_key, languages=dataset['language'], timezone=tz)
+        # TODO: produce a user-visible error message if the location_resource can't be found
+        locations_resource = toolkit.get_action('resource_show')(None, {
+            'id': dataset['locations_resource_id']
         })
 
-    # Add the resource views (like the data preview), they don't get added to empty resources on
-    # creation
-    toolkit.get_action('resource_create_default_resource_views')(None, {
-        'resource': resource,
-        'package': dataset,
-        'create_datastore_views': True
-    })
+        # Pass along the provided cookies so we stay authenticated
+        locations_response = requests.get(
+            locations_resource['url'],
+            cookies=cookies,
+            stream=True
+        )
 
-    logging.info(f'Forecaster complete for dataset {dataset["id"]} resource {resource["id"]}')
+        forecasts = forecaster.run(codecs.iterdecode(locations_response.iter_lines(), 'utf-8'))
+
+        datastore_create = toolkit.get_action('datastore_create')
+
+        for forecast in forecasts:
+            # Add one row at a time so we don't have to store everything in memory
+            datastore_create(None, {
+                'resource_id': resource['id'],
+                'records': [forecast],
+                'force': True
+            })
+
+        # Add the resource views (like the data preview), they don't get added to empty resources on
+        # creation
+        toolkit.get_action('resource_create_default_resource_views')(None, {
+            'resource': resource,
+            'package': dataset,
+            'create_datastore_views': True
+        })
+
+        logging.info(f'Forecasts complete for dataset {dataset["id"]} resource {resource["id"]}')
+    except requests.exceptions.RequestException:
+        logging.exception('Could not fetch locations resource')
+    except Exception:
+        logging.exception('Unexpected error')
 
 
 
